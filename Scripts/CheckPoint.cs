d81674d8f4f88dfa20a65544aaf95daaea62c373
saguilera25-debug <saguilera25@cftsa.cl>
2026-06-16 18:02:33 -0400

    Delete Scripts directory
    Ahora mismo estoy planeando de traer la documentación de algunos códigos, con una descripción al inicio para que sepan para que se utilizan.

--- Scripts/CheckPoint.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class CheckPoint : MonoBehaviour
-{
-    public bool isFinishLine = false;
-    public int checkPointNumber = 1;
-}
